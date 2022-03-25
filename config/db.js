b84a2f684353a4d69f24ec493963ddb8ab6db9d5
StvJo <sjon10422@gmail.com>
2022-03-25 09:24:24 +0700

    fixing uri

--- config/db.js
+++ config/db.js
@@ -1,7 +1,10 @@
 const mongoose = require("mongoose");
 
-const uri = "mongodb://localhost:27018/school";
+// const uri = "mongodb://localhost:27018/school";
 
-const db = mongoose.connect(uri)
+const uri =
+  "mongodb+srv://stvjo:sjon10210221@cluster0.khpm6.mongodb.net/school?retryWrites=true&w=majority";
 
-module.exports = db
+const db = mongoose.connect(uri);
+
+module.exports = db;
